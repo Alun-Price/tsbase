84e34e40c8b80f31055bcd0688e34f2b29ad9b7b
Alun Price <alunprice089@gmail.com>
2023-11-14 02:33:58 +0000

    S5 62 Add addEmployee and printEmployeeInformation methods; private and public modifiers

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,8 +9,23 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Alun");
+accounting.addEmployee("Sinead");
+
 accounting.describe();
+accounting.printEmployeeInformation();
+
+// accounting.employees[2] = "Anna";
