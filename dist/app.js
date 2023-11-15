e8f7654493984d8e08e0dafc7411bd7dd7693248
Alun Price <alunprice089@gmail.com>
2023-11-15 00:41:41 +0000

    S6 84 Intersection Types; Type Guards

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,45 @@
-
+"use strict";
+const e1 = {
+    name: "Alun",
+    privileges: ["create-server"],
+    startDate: new Date(),
+};
+function printEmployeeInformation(emp) {
+    console.log("Name: ", emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges: ", emp.privileges);
+    }
+    if ("startDate" in emp) {
+        console.log("Start Date: ", emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+class Car {
+    drive() {
+        console.log("Driving a car...");
+    }
+}
+class Truck {
+    drive() {
+        console.log("Driving a truck ...");
+    }
+    loadCargo(amount) {
+        console.log("Loading cargo ... Amount: ", amount);
+    }
+}
+const v1 = new Car();
+const v2 = new Truck();
+function useVehicle(vehicle) {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000);
+    }
+}
+useVehicle(v1);
+useVehicle(v2);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,75 @@
+type Admin = {
+  name: string;
+  privileges: string[];
+};
 
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1: ElevatedEmployee = {
+  name: "Alun",
+  privileges: ["create-server"],
+  startDate: new Date(),
+};
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: ", emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: ", emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: ", emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+class Car {
+  drive() {
+    console.log("Driving a car...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck ...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo ... Amount: ", amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  // if ("loadCargo" in vehicle) {
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
