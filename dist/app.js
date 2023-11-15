77b8a3b5a11fefafbc67bac2b0f78f10560eb8a6
Alun Price <alunprice089@gmail.com>
2023-11-15 01:53:12 +0000

    S6 86 Discriminated Unions; Type Casting

--- dist/app.js
+++ dist/app.js
@@ -43,3 +43,18 @@ function useVehicle(vehicle) {
 }
 useVehicle(v1);
 useVehicle(v2);
+function moveAnimal(animal) {
+    let speed;
+    switch (animal.type) {
+        case "bird":
+            speed = animal.flyingSpeed;
+            break;
+        case "horse":
+            speed = animal.runningSpeed;
+    }
+    console.log("Moving with speed: ", speed);
+}
+moveAnimal({ type: "bird", flyingSpeed: 10 });
+moveAnimal({ type: "horse", runningSpeed: 4 });
+const userInputElement = document.getElementById("user-input");
+userInputElement.innerHTML = "Hi there! Casting works!!";
--- src/app.ts
+++ src/app.ts
@@ -73,3 +73,37 @@ function useVehicle(vehicle: Vehicle) {
 
 useVehicle(v1);
 useVehicle(v2);
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+  }
+  console.log("Moving with speed: ", speed);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 10 });
+moveAnimal({ type: "horse", runningSpeed: 4 });
+
+// const userInputElement = <HTMLInputElement>document.getElementById('user-input')
+const userInputElement = document.getElementById(
+  "user-input"
+)! as HTMLInputElement;
+
+userInputElement.innerHTML = "Hi there! Casting works!!";
